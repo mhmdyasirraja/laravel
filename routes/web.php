3c3bc725ea70cf3fe8635b9b8458df1f8cf43692
mhmdyasirraja <mhmdyasir00@gmail.com>
2026-03-09 22:34:33 +0700

    test1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/', [HomeController::class, 'index']);
+route::get('contact', [HomeController::class, 'contact']);
